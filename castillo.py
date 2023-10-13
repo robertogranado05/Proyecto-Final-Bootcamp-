e30b15599715e7f18af222d15127fca943b31eed
Mauricio Zapata <mauricio.zapata@gmail.com>
2023-10-13 16:50:10 -0500

    castillo

--- castillo.py
+++ castillo.py
@@ -10,5 +10,35 @@ imagen.centroX = 100
 
 #CAMINO
 Rect(0, 320, 400, 320, relleno=gradiente('marVerde', 'marVerdeOscuro'))
-Poligono(80, 320, 80, 340, 80, 358, 100, 375, 97, 400, 135, 400, 130, 357, 115, 320,
+Poligono(80, Rect(0, 320, 400, 320, relleno="verde")
+
+#TORRE-DERECHA
+Rect(245, 130, 100, 190, relleno='azulAlica', borde="grisOscuro", anchuraDeBorde=3)
+Poligono(335, 55, 290, 20, 250, 55, relleno='turquesa', borde="grisOscuro")
+Rect(235, 117, 120, 20, relleno='grisOscuro')
+Rect(235, 55, 110, 16, relleno='grisOscuro')
+Rect(255, 70, 76, 50, relleno='azulAlica', borde="grisOscuro")
+Circulo(347, 63, 8, relleno='grisOscuro')
+Circulo(238, 63, 8, relleno='grisOscuro')
+Circulo(354, 127, 10, relleno='grisOscuro')
+Circulo(236, 127, 10, relleno='grisOscuro')
+
+#TORRE-CENTRAL
+Rect(150, 118, 70, 80, relleno='azulAlica', borde="grisOscuro", anchuraDeBorde=3)
+
+#BASE
+Rect(120, 195, 130, 125, relleno='azulAlica', borde="grisOscuro", anchuraDeBorde=5)
+
+#TORRE-IZQUIERDA
+Rect(33, 130, 90, 190, relleno='azulAlica', borde="grisOscuro", anchuraDeBorde=3)
+Rect(40, 70, 76, 50, relleno='azulAlica', borde="grisOscuro")
+Circulo(130, 127, 10, relleno='grisOscuro')
+Circulo(27, 127, 10, relleno='grisOscuro')
+Rect(30, 55, 100, 16, relleno='grisOscuro')
+Circulo(30, 63, 8, relleno='grisOscuro')
+Circulo(129, 63, 8, relleno='grisOscuro')
+Rect(25, 117, 105, 20, relleno='grisOscuro')
+
+Poligono(33, 55, 77, 20, 125, 55, relleno='turquesa', borde="grisOscuro")
+320, 80, 340, 80, 358, 100, 375, 97, 400, 135, 400, 130, 357, 115, 320,
 relleno=gradiente('varillaDoradaClaraAmarilla', 'varillaDoradaPálida', inicio="inferior"))
