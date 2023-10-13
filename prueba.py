36041cbb12ab89fa6c19c3ec6f275ea3ed299178
Maria Del Mar <mariadelmarmosqueracaicedo@gmail.com>
2023-10-13 16:49:40 -0500

    intento 1

--- prueba.py
+++ prueba.py
@@ -2,8 +2,7 @@ import random
 
 ingles = [['Como se dice casa', 'House'], 
             ['como se dice carro', 'car'], 
-            ['como se dice mamá', 'Mom']
-        ]
+            ['como se dice mamá', 'Mom']]
 mates = [['Cual es la raíz cuadrada de 9', '3'], 
         ['cuantos es dos más dos', '4'], 
         ['cuantos es 2 X 4', '8']]
@@ -12,16 +11,27 @@ general = [['¿Cuantos huesos tiene el cuerpo humano?', '206'],
             ['¿En que fecha se celebra la independencia en Colombia?', '20 de Julio']
             ]
 
-listaDePreguntas  = [ingles, mates, general]
 pregunta = ''
 respuesta = ''
+listaElegida = list()
 
-def obtenerPregunta():
-    for lista in listaDePreguntas:
-        for fila in range(3):
-            pregunta = lista[fila][0]
-            respuesta = lista[fila][1]
-            print(pregunta)
+def obtenerPregunta(lista):
+    # for lista in listaDePreguntas:
+    lista.lower()
+    if lista == 'ingles':
+        fila = random.randint(0, 2)
+        pregunta = ingles[fila][0]            
+        respuesta = ingles[fila][1]
+    elif lista == 'matematicas':
+        fila = random.randint(0, 2)
+        pregunta = mates[fila][0]            
+        respuesta = mates[fila][1]
+    elif lista == 'cultura general':
+        fila = random.randint(0, 2)
+        pregunta = general[fila][0]            
+        respuesta = general[fila][1]
 
-while True:
-    obtenerPregunta()
+    print(pregunta)
+
+materia = input("¿Que materia desea escoger entre estas?\n Inglés\n Matemáticas\n Cultural General\n")
+obtenerPregunta(materia)
