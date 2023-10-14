a5b0008dafda80b73020f0266e3fc960d8852365
Tatiana Andrea Pineda <andreatabares905@gmail.com>
2023-10-14 07:18:27 -0500

    SeleccionDeActividades

---
+++ seleccionDeActividades.py
@@ -0,0 +1,37 @@
+Rect(0,0,400,400,relleno=gradiente('nebulosaRosa','varillaDoradaPalida',inicio='superior'),borde='marrónCuero',anchuraDeBorde=5)
+Rect(21,240,120,40,relleno='gainsBoro',borde='marrónCuero')
+Rect(170,240,60,40,relleno='gainsBoro',borde='marrónCuero')
+Rect(260,240,110,40,relleno='gainsBoro',borde='marrónCuero')
+
+
+Rotulo('¿Que actividad prefieres para combatir?',200,200,tamaño=18)
+Rotulo('Cultura general',80,260,tamaño=14)
+Rotulo('Ingles',200,260,tamaño=14)
+Rotulo('Matematicas',315,260,tamaño=14)
+Rotulo('¡Suerte en tú combate!',200,310,tamaño=16)
+Poligono(165,330,165,390,260,360,relleno='gainsBoro',borde='marrónCuero')
+Rotulo('START',200,360,tamaño=20,fuente='monospace',negrito=Verdadero)
+#SIMBOLO RESTA
+Linea(345,25,375,45,relleno='azulVioleta',anchuraDeLinea=6)
+#SIMBOLO SUMA
+Linea(40,320,40,354,relleno ='aguaMarinaMedio',anchuraDeLinea=7)
+Linea(25,337,55,337,relleno ='aguaMarinaMedio',anchuraDeLinea=7)
+#SIMBOLO DIVICION
+Circulo(359,312,5,relleno='azulGandul')
+Linea(340,319,367,329,relleno='azulGandul',anchuraDeLinea=5)
+Circulo(349,336,5,relleno='azulGandul')
+#SIMBOLO MULTIPLICACION
+Linea(146,130,170,160,relleno='coral',anchuraDeLinea=5)
+Linea(170,130,145,160,relleno='coral',anchuraDeLinea=5)
+
+#BANDERA JAPON
+Rect(35,30,65,40,relleno='blanco')
+Circulo(68,50,14,relleno='rojo')
+
+#BANDERA SUECIA
+Rect(280,120,65,40,relleno='azulReal')
+Linea(280,140,345,140,relleno='amarillo',anchuraDeLinea=5)
+Linea(300,120,300,160,relleno='amarillo',anchuraDeLinea=5)
+
+Poligono(215,50,245,50,230,20,relleno='rosadoFuerte')
+Circulo(50,150,18,relleno=gradiente('limaVerde','azulReal','verde',inicio='superior'))
